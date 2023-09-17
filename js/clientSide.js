898f57b0752e9b0d3a0034915799ae33256c0804
Gabriel Viernes <viernesgabriel@gmail.com>
2023-09-17 14:12:15 -0500

    start button hides itself and displays question when clicked

---
+++ js/clientSide.js
@@ -0,0 +1,6 @@
+var startBtn = $("#startBtn").on("click", () => {
+    $("#startBtnCont").addClass("d-none");
+    console.log("startBtn hidden");
+    $("#questionCont").removeClass("d-none");
+})
+const questPlate
