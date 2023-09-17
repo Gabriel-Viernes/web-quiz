a9b8e014701284a1ec4ca115a7166ad3df329bec
Gabriel Viernes <viernesgabriel@gmail.com>
2023-09-17 14:34:24 -0500

    actual questions added to js

--- js/clientSide.js
+++ js/clientSide.js
@@ -3,4 +3,18 @@ var startBtn = $("#startBtn").on("click", () => {
     console.log("startBtn hidden");
     $("#questionCont").removeClass("d-none");
 })
-const questPlate
+function questTemplate(question,a,b,c,d,correct) {
+    this.question = question;
+    this.a = a;
+    this.b = b;
+    this.c = c;
+    this.d = d;
+    this.correct = correct;
+    // correct will be a number from 1-4, corresponding with a-d
+}
+const q1 = new questTemplate("What is used to apply styling to HTML?","HTML", "CSS", "Javascript", "Java", 2);
+const q2 = new questTemplate("What does `=` mean in Javascript?","equals","is operator","assignment operator", "equals operator", 3);
+const q3 = new questTemplate("What is your best resource for researching methods for a given API?","The API's documentation","Stack Overflow","Google","ChatAPT", 1);
+const q4 = new questTemplate("What is KISS?", "A Band","Keep Infrastructure Solid State","Keep In Solid Snake","Keep It Simple Stupid",4);
+const q5 = new questTemplate("Which of the following is used to make a webpage dynamic?", "Java", "Javascipt","C","BASIC", 1);
+const questionList = [q1,q2,q3,q4,q5];
